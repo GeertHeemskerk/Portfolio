d01365cc92d385b59dc7e7c15d509f6af7598c47
Geert Heemskerk <leaguecrafterhd@gmail.com>
2016-10-05 11:52:04 +0200

    added some tips from kim

--- index.php
+++ index.php
@@ -1,68 +1,55 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Portfolio Geert Heemskerk</title>
         <link rel="stylesheet" type="text/css" href="src/css/index.css">
-        <script rel="src/js/index.js" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Monda" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <header class="jumbotron">
                 <h1 class="jumbotron-header">Geert Heemskerk</h1>
-                <h1 class="jumbotron-subject">Developer</h1>
+                <h2 class="jumbotron-subject">Developer</h1>
                 <div class="profile-picture">
                 </div>
             </header>
             <div class="info-wrapper">
-                <h1 class="info-wrapper-header">About</h1>
+                <h2 class="info-wrapper-header">About</h1>
                 <div class="info-container-left">
-                  <h1>About me</h1>
-                  <p class="wrapper-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                  <h2>About me</h2>
+                  <p class="wrapper-content">Hi!, My name is Geert Heemskerk I'm 18 years old. I am currently on an intership at Competa IT located in Rijswijk, this is my second periode of intership (2 times half a year). I am still learning so much on this intership about new stuff like frameworks and methods, I am in my third year of school, that is also the last year of my MBO 4 Application developer education. After this education I probably go for an eduction simmular like the one i am doing right now. But I think I'm going towards an eduction that focusses on design or interaction with clients.</p>
                 </div>
                     <div class="info-container-right">
-                      <h1>Overall experience</h1>
-                      <p class="wrapper-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                      <h2>Overall experience</h2>
+                      <p class="wrapper-content">I am going to tell a short story about my overall experience with development. I worked on different projects on school and most of all at Competa IT. For example we worked on a project for HHS (Haagse Hoge School), we had to work with back-end kenyans, so we had to talk english and via slack. sometimes there were miscommunications and we had to solve them. I learned that you have to be patient to achieve goals. I had to learn new develop languages to achive the goals that needed to be achived! I'm very driven to learn new languages and participate in projects, questions?? contact me below!.</p>
                     </div>
                     <div class="info-container-bottom">
-                      <h1>Skills</h1>
+                      <h2>Skills</h2>
                         <div class="skillsLeft">
-                            <p>HTML(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
-                            <p>CSS/SASS(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
-                            <p>Javascript(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
-                            <p>Photoshop(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
+                          <img class="skillsImages" src="img/skills-left.png" alt="skillsLeft">
                         </div>
+                          <img class="skills-middle-image" src="img/skills-middle.png" alt="skillsmiddle">
                         <div class="skillsRight">
-                            <p>Jquery(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
-                            <p>JAVA(5%)</p>
-                            <progress class="progressbar" value="5" max="100" ></progress>
-                            <p>PHP(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
-                            <p>MySQL(5%)</p>
-                            <progress class="progressbar" value="5" max="100"></progress>
+                          <img class="skillsImages" src="img/skills-right.png" alt="skillsRight">
                         </div>
                     </div>
                 </div>
-                <header class="jumbotron">
-                </header>
+                <section class="jumbotron">
+                </section>
                 <form class="contact-form">
-                  <h1 class="contact-header">Contact</h1>
-                     <p>Full name</p>
-                     <input class="input-name" name="name" type="text" placeholder="John Doe"/>
-                     <p>Email</p>
-                     <input class="input-email" name="email" type="text" placeholder="JohnDoe@gmail.com"/>
-                     <p>Subject</p>
-                     <input class="input-subject" name="subject" type="text" placeholder="Subject..."/>
-                     <p>Comment</p>
-                     <textarea class="input-comment" name="email" type="text" placeholder="I want to contact you because..."></textarea>
+                  <h2 class="contact-header">Contact</h2>
+                     <label>Full name</label>
+                     <input class="input-name" name="name" type="text" placeholder="John Doe" required/>
+                     <label>Email</label>
+                     <input class="input-email" name="email" type="text" placeholder="JohnDoe@gmail.com" required/>
+                     <label>Subject</label>
+                     <input class="input-subject" name="subject" type="text" placeholder="Subject..."required/>
+                     <label>Comment</label>
+                     <textarea class="input-comment" name="email" type="text" placeholder="I want to contact you because..."required></textarea>
                      <button class="send" type="submit" form="contact-form" value="Submit">Send</button>
                 </form>
             </div>
             <a href="#top" class="scrollToTop">Scroll To Top</a>
+        <script rel="src/js/index.js" type="text/javascript"></script>
     </body>
 </html>
