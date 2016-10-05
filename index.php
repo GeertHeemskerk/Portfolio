a5e53619ca4512dde4522cf4d57eda6e3a544417
Geert Heemskerk <leaguecrafterhd@gmail.com>
2016-10-05 15:56:51 +0200

    Made a small change to skill section (deciding if skill is new section or stays with about me)

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
                 </div>
             </header>
             <div class="info-wrapper">
-                <h2 class="info-wrapper-header">About</h1>
+                <h2 class="info-wrapper-header">About</h2>
                 <div class="info-container-left">
                   <h2>About me</h2>
                   <p class="wrapper-content">Hi!, My name is Geert Heemskerk I'm 18 years old. I am currently on an intership at Competa IT located in Rijswijk, this is my second periode of intership (2 times half a year). I am still learning so much on this intership about new stuff like frameworks and methods, I am in my third year of school, that is also the last year of my MBO 4 Application developer education. After this education I probably go for an eduction simmular like the one i am doing right now. But I think I'm going towards an eduction that focusses on design or interaction with clients.</p>
@@ -49,7 +49,7 @@
                       <p class="wrapper-content">I am going to tell a short story about my overall experience with development. I worked on different projects on school and most of all at Competa IT. For example we worked on a project for HHS (Haagse Hoge School), we had to work with back-end kenyans, so we had to talk english and via slack. sometimes there were miscommunications and we had to solve them. I learned that you have to be patient to achieve goals. I had to learn new develop languages to achive the goals that needed to be achived! I'm very driven to learn new languages and participate in projects, questions?? contact me below!.</p>
                     </div>
                     <div class="info-container-bottom">
-                      <h2>Skills</h2>
+                      <h2 class="info-wrapper-header">Skills</h2>
                         <div class="skillsLeft">
                           <img class="skillsImages" src="img/skills-left.png" alt="skillsLeft">
                         </div>
